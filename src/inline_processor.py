f2a007cb9cfb283baf9cda5d7da14ce04b8de8b7
Anthony Spencer <josephs@smu.edu>
2025-02-12 18:31:46 -0700

    Implemented split_node_delimiter

--- src/inline_processor.py
+++ src/inline_processor.py
@@ -10,10 +10,11 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             if len(splitted) % 2 == 1:
                 if node.text[0] == delimiter:
                     node_to_list(splitted, new_nodes, text_type)
-                    splitted = splitted[1:]
+                    new_nodes = new_nodes[1:]
+                    
                 elif node.text[-1] == delimiter:
                     node_to_list(splitted, new_nodes, text_type)
-                    splitted = splitted[:-1]
+                    new_nodes = new_nodes[:-1]
                 else:
                     node_to_list(splitted, new_nodes, text_type)
             else:
--- src/test_inlineprocessor.py
+++ src/test_inlineprocessor.py
@@ -4,5 +4,49 @@ from inline_processor import split_nodes_delimiter
 from textnode import TextNode, TextType
 
 class TestInlineProcessor(unittest.TestCase):
+    def test_split_nodes_delimiter_1_code(self):
+        node = TextNode("This is text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.CODE),
+            TextNode(" word", TextType.TEXT),
+        ])
+
     def test_split_nodes_delimiter_1_bold(self):
-        pass
+        node = TextNode("This is text with a *code block* word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ])
+
+    def test_split_nodes_delimiter_2_bold_front(self):
+        node = TextNode("*This* is text with a *code block* word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This", TextType.BOLD),
+            TextNode(" is text with a ", TextType.TEXT),
+            TextNode("code block", TextType.BOLD),
+            TextNode(" word", TextType.TEXT),
+        ])
+    
+    def test_split_nodes_delimiter_2_bold_back(self):
+        node = TextNode("This is text with a *code* block *word*", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "*", TextType.BOLD)
+        self.assertEqual(new_nodes, [
+            TextNode("This is text with a ", TextType.TEXT),
+            TextNode("code", TextType.BOLD),
+            TextNode(" block ", TextType.TEXT),
+            TextNode("word", TextType.BOLD),
+        ])
+    
+    def test_split_nodes_delimite_missing_closing_character(self):
+        threw = False
+        try:
+            node = TextNode("This is text with a *code block word", TextType.TEXT)
+            new_nodes = split_nodes_delimiter([node], "*", TextType.BOLD)
+        except:
+            threw = True
+            self.assertTrue(threw)
