4206f302a8beab507207302f475d69e048a8bad9
Anthony Spencer <josephs@smu.edu>
2025-03-01 12:09:33 -0700

    built site to docs in order to deploy to GHPages

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ from htmlnode import HTMLNode, ParentNode, LeafNode
 from document_processor import *
 import os
 import shutil
+import sys
 
 
 def extract_title(markdown):
@@ -13,7 +14,7 @@ def extract_title(markdown):
 	raise Exception("No Header Found in markdown file")
 
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
 	print(f'Generating page from {from_path} to {dest_path} using template {template_path}')
 
 	if not os.path.exists(from_path):
@@ -41,13 +42,14 @@ def generate_page(from_path, template_path, dest_path):
 
 	title_filled_template = template.replace("{{ Title }}", title)
 	filled_template = title_filled_template.replace("{{ Content }}", HTML)
+	base_filled_template = filled_template.replace('href="/', f'href="{basepath}').replace('src="/', f'src="{basepath}')
 
 	if not os.path.exists(os.path.dirname(dest_path)):
 		#print("THE PATH YOU SEEK: " + os.path.dirname(dest_path))
 		os.makedirs(os.path.dirname(dest_path))
 
 	with open(dest_path,'w') as file:
-		file.write(filled_template)
+		file.write(base_filled_template)
 
 	print(f"Content successfuly written to {dest_path}")
 	
@@ -55,10 +57,11 @@ def generate_page(from_path, template_path, dest_path):
 
 def copy_from_src_to_dest_dir(src, dest):
 	try:
-		if not os.path.exists(dest):
-			raise FileExistsError("Destination Dir does not exist")
+		
 		if not os.path.exists(src):
 			raise FileExistsError("Source Dir does not exist")
+		if not os.path.exists(dest):
+			os.makedirs(dest)
 		
 		if os.path.isfile(dest):
 			raise NotADirectoryError("Dest is not a directory")
@@ -104,7 +107,7 @@ def recursive_copy(src_file, dest, template="./template.html"):
 			os.mkdir(dest_path)
 			recursive_copy(file_path,dest_path)
 
-def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
+def generate_pages_recursive(dir_path_content, template_path, dest_dir_path, basepath):
 	src_contents = os.listdir(dir_path_content)
 
 	
@@ -119,21 +122,21 @@ def generate_pages_recursive(dir_path_content, template_path, dest_dir_path):
 				print("Copy successful")
 			else:
 				dest_path = os.path.join(dest_dir_path, f"{file_name}.html")
-				generate_page(file_path,template_path, dest_path)
+				generate_page(file_path,template_path, dest_path, basepath)
 		else:
 			dest_path = os.path.join(dest_dir_path,file)
 			print("Creating new dest folder: " + dest_path)
 			os.mkdir(dest_path)
-			generate_pages_recursive(file_path,template_path, dest_path)
+			generate_pages_recursive(file_path,template_path, dest_path,basepath)
 
 
-def initialize_site():
-	dest = "./public"
-	src = "./static"
+def initialize_site(basepath):
+	dest = "docs"
+	src = "static"
 
 	copy_from_src_to_dest_dir(src, dest)
 
-	generate_pages_recursive("./content","./template.html",dest)
+	generate_pages_recursive("content","template.html",dest, basepath)
 
 	
 
@@ -151,6 +154,13 @@ def main():
 	contents = os.listdir("./content")
 
 	generate_page("./content/index.md", "./template.html","./public/index.html") """
-	initialize_site()
+	basepath = '/'
+	if len(sys.argv) > 1:
+		if sys.argv[1]:
+			basepath = sys.argv[1]
+		else:
+			basepath = '/'
+	print(basepath)
+	initialize_site(basepath)
 
 main()
